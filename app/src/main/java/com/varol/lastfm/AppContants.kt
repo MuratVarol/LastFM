5ca4d57f95cd1cfe186002844b14b9e6d01c1ed8
varol <muratv@anadolu.edu.tr>
2019-08-07 09:40:50 +0300

    lastfm link added

--- app/src/main/java/com/varol/lastfm/AppContants.kt
+++ app/src/main/java/com/varol/lastfm/AppContants.kt
@@ -1,5 +1,5 @@
 package com.varol.lastfm
 
-const val BASE_LINK = "https://newsapi.org"
+const val BASE_LINK = "http://ws.audioscrobbler.com/2.0/"
 
-const val APP_DATABASE_NAME = "newsDatabase"
+const val APP_DATABASE_NAME = "lastFmDb"
