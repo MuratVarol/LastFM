c3ee16f482b5bf15c8efd0fdd81609f3d76e227f
varol <muratv@anadolu.edu.tr>
2019-08-07 17:23:21 +0300

    vm and resources added for search artist page

---
+++ app/src/main/java/com/varol/lastfm/ui/SearchArtistFragment.kt
@@ -0,0 +1,27 @@
+package com.varol.lastfm.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.varol.lastfm.R
+import com.varol.lastfm.base.BaseFragment
+import com.varol.lastfm.databinding.FragmentSearchBinding
+import com.varol.lastfm.viewmodel.SearchArtistsVM
+
+class SearchArtistFragment :
+    BaseFragment<SearchArtistsVM, FragmentSearchBinding>(SearchArtistsVM::class) {
+    override val getLayoutId: Int = R.layout.fragment_search
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+
+        return binding.root
+    }
+
+}
---
+++ app/src/main/java/com/varol/lastfm/viewmodel/SearchArtistsVM.kt
@@ -0,0 +1,50 @@
+package com.varol.lastfm.viewmodel
+
+import android.util.Log
+import android.view.View
+import androidx.lifecycle.MutableLiveData
+import com.varol.lastfm.base.BaseVM
+import com.varol.lastfm.data.local.model.ArtistModel
+import com.varol.lastfm.data.remote.DataHolder
+import com.varol.lastfm.usecase.SearchArtistUseCase
+import com.varol.lastfm.util.listener.ItemClickListener
+
+
+class SearchArtistsVM(
+    private val searchArtistUseCase: SearchArtistUseCase
+) : BaseVM() {
+
+    val searchText = MutableLiveData<String>()
+
+    val artistSearchList = MutableLiveData<List<ArtistModel>>()
+
+    val itemClickListener = object : ItemClickListener<ArtistModel> {
+        override fun onItemClick(view: View, item: ArtistModel, position: Int) {
+            Log.i("ArtistModel: ", item.toString())
+        }
+
+    }
+
+    fun searchArtist() {
+
+        searchText.value?.let { name ->
+            val disposable = searchArtistUseCase
+                .searchArtist(name)
+                .observeOn(getBackgroundScheduler())
+                .subscribeOn(getBackgroundScheduler())
+                .subscribe { data ->
+                    when (data) {
+                        is DataHolder.Success -> {
+                            artistSearchList.postValue(data.data.results.artistmatches.artist)
+                        }
+                        else -> {
+                        }
+                    }
+                }
+            disposables.add(disposable)
+        }
+
+
+    }
+
+}
