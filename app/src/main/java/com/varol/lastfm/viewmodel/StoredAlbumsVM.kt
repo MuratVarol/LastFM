d156ca1f04c340b55eff00e6addba2b92c636639
varol <muratv@anadolu.edu.tr>
2019-08-07 17:23:49 +0300

    stored album viewmodel created

--- app/src/main/java/com/varol/lastfm/viewmodel/StoredAlbumsVM.kt
+++ app/src/main/java/com/varol/lastfm/viewmodel/StoredAlbumsVM.kt
@@ -1,24 +1,6 @@
 package com.varol.lastfm.viewmodel
 
 import com.varol.lastfm.base.BaseVM
-import com.varol.lastfm.usecase.SearchArtistUseCase
 
 
-class StoredAlbumsVM(
-    private val searchArtistUseCase: SearchArtistUseCase
-) : BaseVM() {
-
-    init {
-
-        val disposable = searchArtistUseCase
-            .searchArtist("cher")
-            .observeOn(getBackgroundScheduler())
-            .subscribeOn(getBackgroundScheduler())
-            .subscribe { t1, t2 ->
-                t1
-            }
-        disposables.add(disposable)
-
-    }
-
-}
+class StoredAlbumsVM : BaseVM()
