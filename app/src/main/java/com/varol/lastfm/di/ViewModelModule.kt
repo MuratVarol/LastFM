0c10a0f79fd7c5d0ef5ed2b5878c44eea08e8dd3
varol <muratv@anadolu.edu.tr>
2019-08-07 17:24:53 +0300

    SearchArtistsVM added to viewModelModule

--- app/src/main/java/com/varol/lastfm/di/ViewModelModule.kt
+++ app/src/main/java/com/varol/lastfm/di/ViewModelModule.kt
@@ -1,6 +1,7 @@
 package com.varol.lastfm.di
 
 import com.varol.lastfm.viewmodel.MainVM
+import com.varol.lastfm.viewmodel.SearchArtistsVM
 import com.varol.lastfm.viewmodel.StoredAlbumsVM
 import org.koin.androidx.viewmodel.ext.koin.viewModel
 import org.koin.dsl.module.Module
@@ -8,5 +9,6 @@ import org.koin.dsl.module.module
 
 val viewModelModule: Module = module {
     viewModel { MainVM() }
-    viewModel { StoredAlbumsVM(get()) }
+    viewModel { StoredAlbumsVM() }
+    viewModel { SearchArtistsVM(get()) }
 }
