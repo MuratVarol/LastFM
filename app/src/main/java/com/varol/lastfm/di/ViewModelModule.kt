551454c271a979e5ada59c32336f37db90050142
varol <muratv@anadolu.edu.tr>
2019-08-08 12:56:46 +0300

    ArtistsVM added and deleted vm removed from module

--- app/src/main/java/com/varol/lastfm/di/ViewModelModule.kt
+++ app/src/main/java/com/varol/lastfm/di/ViewModelModule.kt
@@ -1,14 +1,14 @@
 package com.varol.lastfm.di
 
+import com.varol.lastfm.viewmodel.AlbumsVM
+import com.varol.lastfm.viewmodel.ArtistsVM
 import com.varol.lastfm.viewmodel.MainVM
-import com.varol.lastfm.viewmodel.SearchArtistsVM
-import com.varol.lastfm.viewmodel.StoredAlbumsVM
 import org.koin.androidx.viewmodel.ext.koin.viewModel
 import org.koin.dsl.module.Module
 import org.koin.dsl.module.module
 
 val viewModelModule: Module = module {
     viewModel { MainVM() }
-    viewModel { StoredAlbumsVM() }
-    viewModel { SearchArtistsVM(get()) }
+    viewModel { ArtistsVM(get()) }
+    viewModel { AlbumsVM(get()) }
 }
