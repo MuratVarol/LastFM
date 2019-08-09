8d9e1b008833fb03b6510b2177a716a472dd8417
varol <muratv@anadolu.edu.tr>
2019-08-09 11:04:32 +0300

    room object re-arranged

--- app/src/main/java/com/varol/lastfm/data/local/model/AlbumDo.kt
+++ app/src/main/java/com/varol/lastfm/data/local/model/AlbumDo.kt
@@ -9,23 +9,25 @@ import com.google.gson.annotations.SerializedName
 @Entity(tableName = "tb_albums")
 data class AlbumDo(
 
-    @ColumnInfo(name = "name")
-    @SerializedName("name")
-    val name: String?,
-
     @PrimaryKey
     @ColumnInfo(name = "id")
     @SerializedName("mbid")
-    val mbid: String?,
+    val mbid: String,
+
+    @ColumnInfo(name = "name")
+    @SerializedName("name")
+    val name: String?,
 
     @ColumnInfo(name = "artist")
     @SerializedName("artist")
     val artist: String?,
 
+    @ColumnInfo(name = "image")
     @SerializedName("image")
     val image: String?,
 
+    @ColumnInfo(name = "tracks")
     @SerializedName("tracks")
-    val tracks: List<String?>?
+    val tracks: List<String>?
 
 )
