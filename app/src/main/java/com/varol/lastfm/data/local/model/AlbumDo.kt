7f6886ee5a30c96c050da137c489797e8a878c44
varol <muratv@anadolu.edu.tr>
2019-08-11 02:21:24 +0300

    model parcelable

--- app/src/main/java/com/varol/lastfm/data/local/model/AlbumDo.kt
+++ app/src/main/java/com/varol/lastfm/data/local/model/AlbumDo.kt
@@ -1,11 +1,14 @@
 package com.varol.lastfm.data.local.model
 
+import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
 
 
+@Parcelize
 @Entity(tableName = "tb_albums")
 data class AlbumDo(
 
@@ -28,6 +31,6 @@ data class AlbumDo(
 
     @ColumnInfo(name = "tracks")
     @SerializedName("tracks")
-    val tracks: List<String>?
+    val tracks: List<String?>?
 
-)
+) : Parcelable
